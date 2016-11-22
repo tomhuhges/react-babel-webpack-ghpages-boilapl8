a9b658e623d287d944619093be7c2d64c293ed24
tomhuhges <merzbear@gmail.com>
2016-11-22 23:59:43 +0000

    init

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const validate = require('webpack-validator');
+const dev = require('./webpack.dev.config');
+const prod = require('./webpack.prod.config');
+
+var paths = {
+  src: __dirname + '/src',
+  build: __dirname + '/docs',
+  css: __dirname + '/src/css'
+};
+
+var defaults = {
+  entry: {
+    app: paths.src,
+    style: paths.css + '/style.css'
+  },
+  output: {
+    path: paths.build
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack demo'
+    })
+  ]
+};
+
+if ( process.env.npm_lifecycle_event === "build" ) {
+
+  // build config
+  var config = merge(
+    defaults, 
+    prod.config({
+      paths: paths
+    })
+  );
+
+} else {
+
+  // dev config
+  var config = merge(
+    defaults,
+    dev.config({
+      paths: paths,
+      server: {
+        host: process.env.HOST,
+        port: process.env.PORT
+      }
+    })
+  );
+}
+
+module.exports = validate(config);
---
+++ webpack.prod.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+exports.config = function(options) {
+  return {
+    output: {
+      publicPatch: '/webpack-test/'
+      filename: '[name].[chunkhash].js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('style', 'css'),
+          include: options.paths.css
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin( [options.paths.build], {
+        root: process.cwd()
+      }),
+      new ExtractTextPlugin('[name].[chunkhash].css')
+    ]
+  };
+}
